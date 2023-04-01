c98717b7c5153485661742b8b3be6d263e9188d6
TARUN <77455621+taranshk@users.noreply.github.com>
2023-04-01 23:30:27 +0530

    Update Circle.jsx

--- src/componets/Circle/Circle.jsx
+++ src/componets/Circle/Circle.jsx
@@ -1,20 +1,39 @@
+// Importing the React library
 import React from "react";
+
+// Importing the Circle.css file
 import "./Circle.css";
+
+// Importing the Timeline and Bubbles components
 import Timeline from "../Timeline/TimeLine";
-function DashboardContent(){
-    return(
-        <>
-        <div class="container1">
-          <div class="parent">
-             <div class="timeline__header">
-               <Timeline/>
-             </div>
-            <div class="child">
-              
+import Bubbles from "../Bubble/bubble";
+
+// Defining a function named DashboardContent
+function DashboardContent() {
+
+  // Returning JSX
+  return (
+    <>
+      {/* A container1 div */}
+      <div class="container1">
+        
+        {/* A parent div */}
+        <div class="parent">
+          
+          {/* A timeline__header div with a Timeline component */}
+          <div class="timeline__header">
+            <Timeline />
           </div>
-         </div>
-       </div>
-        </>
-    );
+          
+          {/* A child div with a Bubbles component */}
+          <div class="child">
+            <Bubbles />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
+
+// Exporting the DashboardContent function as the default export
 export default DashboardContent;
