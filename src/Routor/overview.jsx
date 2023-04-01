9d0cdfe023f1ad25741988cfdf6c1f356c64e5c2
TARUN <77455621+taranshk@users.noreply.github.com>
2023-04-01 23:26:06 +0530

    Update overview.jsx

--- src/Routor/overview.jsx
+++ src/Routor/overview.jsx
@@ -1,11 +1,16 @@
+// Import necessary modules from React
 import React from "react";
 
+// Define a functional component called over
 const over= ()=> {
-    return(
-        <>
-        <h1>hello i m overview page</h1>
-        </>
-    );
+// Return the following JSX code
+return(
+<>
+<h1>hello i m overview page</h1>
+</>
+// Close the JSX code
+)
 
 };
+// Export the over component as the default export
 export default over;
