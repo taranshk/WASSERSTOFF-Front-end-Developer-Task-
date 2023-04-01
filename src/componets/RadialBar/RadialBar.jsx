6ccdbc581a0450c6791c00c3ced7476377dabc3f
TARUN <77455621+taranshk@users.noreply.github.com>
2023-04-01 23:44:36 +0530

    Update RadialBar.jsx

--- src/componets/RadialBar/RadialBar.jsx
+++ src/componets/RadialBar/RadialBar.jsx
@@ -1,8 +1,17 @@
+// This imports the RadialBar component from the "@ant-design/plots" library.
 import { RadialBar } from "@ant-design/plots";
-/*import React, { useState, useEffect } from "react";
-import ReactDOM from "react-dom";*/
+
+/* This imports React, useState, useEffect, and ReactDOM.
+   However, these imports are not currently being used in this file, so they have been commented out. */
+// import React, { useState, useEffect } from "react";
+// import ReactDOM from "react-dom";
+
+// This imports the "radialbar.css" file, which contains styling for this component.
 import "./radialbar.css";
+
+// This defines a functional component named SiderRadialBar.
 const SiderRadialBar = () => {
+  // This defines an array of objects containing data for the chart.
   const data = [
     {
       name: "G6",
@@ -17,14 +26,17 @@ const SiderRadialBar = () => {
       star: 10178
     }
   ];
+
+  // This defines the configuration options for the RadialBar component.
   const config = {
-    data,
-    xField: "name",
-    yField: "star",
-    maxAngle: 150,
-   
-    radius: 0.6,
-    innerRadius: 0.2,
+    data, // This sets the data for the chart.
+    xField: "name", // This sets the data field used for the x-axis.
+    yField: "star", // This sets the data field used for the y-axis.
+    maxAngle: 150, // This sets the maximum angle of the chart.
+    radius: 0.6, // This sets the outer radius of the chart.
+    innerRadius: 0.2, // This sets the inner radius of the chart.
+    
+    // This defines the tooltip for the chart.
     tooltip: {
       formatter: (datum) => {
         return {
@@ -33,6 +45,8 @@ const SiderRadialBar = () => {
         };
       }
     },
+
+    // This sets the color of the bars based on the number of stars.
     colorField: "star",
     color: ({ star }) => {
       if (star > 10000) {
@@ -40,13 +54,17 @@ const SiderRadialBar = () => {
       } else if (star > 1000) {
         return "#ff9300";
       }
-
       return "#ff93a7";
     },
-    barBackground: {},
+
+    barBackground: {}, // This sets the background of the bars.
+    
+    // This sets the style for the bars.
     barStyle: {
       lineCap: "round"
     },
+
+    // This adds an annotation to the chart.
     annotations: [
       {
         type: "html",
@@ -63,7 +81,10 @@ const SiderRadialBar = () => {
       }
     ]
   };
+
+  // This returns the RadialBar component with the configuration options passed as props.
   return <RadialBar {...config} />;
 };
 
+// This exports the SiderRadialBar component so that it can be used in other files.
 export default SiderRadialBar;
