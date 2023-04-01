a7bef25644fcbd7429133b79f17c6462f82b1763
TARUN <77455621+taranshk@users.noreply.github.com>
2023-04-01 23:34:36 +0530

    Update SubHeader.jsx

--- src/componets/Header/SubHeader.jsx
+++ src/componets/Header/SubHeader.jsx
@@ -1,14 +1,31 @@
+// Importing the React library
 import React from "react";
+
+// Importing Logo, SearchBar, Navigation, and Preferences components
 import Logo from "../Logo/Logo";
 import SearchBar from "../SearchBar/SearchBar";
 import Navigation from "../Nav/Navigation";
 import Preferences from "../Preference/Preferences";
+
+// Defining a constant variable named SubHeader as a functional component
 const SubHeader = () => (
+    // A header element with a class name "header"
     <header className="header">
+      
+      {/* Rendering the Logo component */}
       <Logo />
+      
+      {/* Rendering the SearchBar component */}
       <SearchBar />
+      
+      {/* Rendering the Navigation component */}
       <Navigation />
+      
+      {/* Rendering the Preferences component */}
       <Preferences/>
+      
     </header>
-  );
-  export default SubHeader;
+);
+
+// Exporting the SubHeader component as the default export
+export default SubHeader;
