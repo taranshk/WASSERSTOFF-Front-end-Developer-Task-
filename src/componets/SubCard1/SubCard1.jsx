f01185f9f57a39f2a56046c43466de2b7841eb0d
TARUN <77455621+taranshk@users.noreply.github.com>
2023-04-01 23:47:10 +0530

    Update SubCard1.jsx

--- src/componets/SubCard1/SubCard1.jsx
+++ src/componets/SubCard1/SubCard1.jsx
@@ -1,16 +1,23 @@
-import { Card } from 'antd';
 import {BiUpArrow} from 'react-icons/bi';
 import "./subcard1.css";
 import SiderRadialBar from '../RadialBar/RadialBar';
+import data from "../Json/data.json";
+
+/* SubCard1 component: This component displays a Card with 
+total earnings and an up arrow percentage.
+the React icons BiUpArrow component, 
+the SiderRadialBar component, and data from a JSON file.*/
+
 const SubCard1 = () => (
-    <Card    style={{
-        width: 300,
-      }}>
+   <> 
+   <div className='subcard__container'>
      <SiderRadialBar/>
      <div className='Card__wrapper'>
      <p>Total earning</p>
-     <p>₹10,57,739 <span className='Arrow'><BiUpArrow/> 2%</span></p>
+     <p>${data.totalEarning} <span className='Arrow'><BiUpArrow/> 2%</span></p>
     </div>
-    </Card>
+    </div>
+    </>
 );
+
 export default SubCard1;
