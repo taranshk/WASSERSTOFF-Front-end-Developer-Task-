a3e27778102ee079193202fdc10b4fdc9c4fc7b3
TARUN <77455621+taranshk@users.noreply.github.com>
2023-04-01 23:35:02 +0530

    Update Logo.jsx

--- src/componets/Logo/Logo.jsx
+++ src/componets/Logo/Logo.jsx
@@ -1,9 +1,22 @@
+// Importing the React library
 import React from "react";
+
+// Importing the CSS file for the Logo component
 import "./logo.css";
+
+// Defining a constant variable named Logo as a functional component that takes in a prop called "src"
 const Logo = ({ src }) => (
+  
+    // A div element with a class name "logo"
     <div className="logo">
+      
+      {/* An image element with a source URL and alt text */}
       <img src={"https://www.thewasserstoff.com/images/logo-top.png"} alt="Logo" width="20px" height="20px" />
-       WASSERSTOFF
+      
+      {/* A text saying "WASSERSTOFF" */}
+      WASSERSTOFF
     </div>
   );
+
+// Exporting the Logo component as the default export
 export default Logo;
