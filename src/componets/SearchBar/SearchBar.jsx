fa80f98ddb0f8cdc3911ed0c0135fe91b7ed91d6
TARUN <77455621+taranshk@users.noreply.github.com>
2023-04-01 23:45:20 +0530

    Update SearchBar.jsx

--- src/componets/SearchBar/SearchBar.jsx
+++ src/componets/SearchBar/SearchBar.jsx
@@ -1,18 +1,23 @@
 import React from "react";
-import "./searchbar.css";
-import { Input, Space } from 'antd';
-const { Search } = Input;
+import "./searchbar.css"; // Importing the styles for the search bar component
+import { Input, Space } from 'antd'; // Importing Ant Design Input and Space components
+const { Search } = Input; // Destructuring the Ant Design Input component to use Search separately
+
+// Function to handle the search
 const onSearch = (value) => console.log(value);
+
+// The SearchBar component
 const SearchBar = () => (
   <Space direction="vertical">
+    {/* The search bar with placeholder text and onSearch function */}
     <Search className="SearchBar"
       placeholder="input search text"
       onSearch={onSearch}
       style={{
         width: 450,
-    
       }}
     />
-</Space>
+  </Space>
 );
-  export default SearchBar;
+
+export default SearchBar;
