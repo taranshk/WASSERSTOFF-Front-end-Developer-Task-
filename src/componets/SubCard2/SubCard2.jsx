53b5e07cd52edcfac9658f74b3ffe9cca39c7ea9
TARUN <77455621+taranshk@users.noreply.github.com>
2023-04-01 23:47:35 +0530

    Update SubCard2.jsx

--- src/componets/SubCard2/SubCard2.jsx
+++ src/componets/SubCard2/SubCard2.jsx
@@ -1,20 +1,27 @@
-import { Card } from 'antd';
+// Importing required modules
 import {MdArrowDropUp} from 'react-icons/md';
 import "./subcard2.css";
+import data from "../Json/data.json";
+
+// Creating functional component SubCard2
 const SubCard2 = () => (
-    <Card style={{
-        width: 300,
-      }}>
-        <div className="SubCard2" style={{ display: 'flex', flexDirection: 'column' }}>
-        <div style={{ flex: 1 }}>
-          <p>Total Earning</p>
-          <p>₹10,57,739<span><MdArrowDropUp className="my-icon-up"/> 12%</span></p>
-        </div>
-        <div style={{ flex: 1 }}>
-          <p>Sales</p>
-          <p>₹36,06,507<span ><MdArrowDropUp className="my-icon-up"/> 10%</span></p>
-        </div>
-      </div>
-    </Card>
+
+
+<div className="SubCard2" style={{ display: 'flex', flexDirection: 'column' }}>
+{/*First content block with flex of 1*/}
+<div style={{ flex: 1 }}>
+<p>Total Earning</p>
+{/* Displaying total earning from the data file and an icon with 12% increase*/}
+<p>${data.totalEarning}<span><MdArrowDropUp className="my-icon-up"/> 12%</span></p>
+</div>
+ {/*Second content block with flex of 1*/}
+<div style={{ flex: 1 }}>
+<p>Sales</p>
+{/*Displaying sales amount from the data file and an icon with 10% increase*/}
+<p>${data.bsales}<span ><MdArrowDropUp className="my-icon-up"/> 10%</span></p>
+</div>
+</div>
+// Closing the fragment
 );
+// Exporting the SubCard2 component
 export default SubCard2;
