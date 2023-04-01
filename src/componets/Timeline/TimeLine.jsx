100192cb335730feb5537139bb0b979c96ed7cc9
TARUN <77455621+taranshk@users.noreply.github.com>
2023-04-01 23:49:16 +0530

    Update TimeLine.jsx

--- src/componets/Timeline/TimeLine.jsx
+++ src/componets/Timeline/TimeLine.jsx
@@ -1,21 +1,25 @@
 import React from "react";
-import BelowTimeline from "../BelowTimeline/Belowtimeline";
-import "./timeline.css";
-const Timeline = () =>{
-    return(
-        <>
-<div class="container">
-    <div className="txt">TIMELINE</div>
-  <div class="button__container">
-    <button class="button">1W</button>
-    <button class="button">1M</button>
-    <button class="button button--active">3M</button>
-    <button class="button">1Y</button>
-    <button class="button">ALL</button>
-  </div>
-</div>
+import "./timeline.css"; // Importing the styles for the timeline component
 
-        </>
-    );
+const Timeline = () => {
+  return (
+    <>
+      {/* The container for the timeline */}
+      <div class="container">
+        {/* The title of the timeline */}
+        <div className="txt">TIMELINE</div>
+        {/* The button container */}
+        <div class="button__container">
+          {/* Buttons to filter the timeline */}
+          <button class="button">1W</button>
+          <button class="button">1M</button>
+          <button class="button button--active">3M</button>
+          <button class="button">1Y</button>
+          <button class="button">ALL</button>
+        </div>
+      </div>
+    </>
+  );
 };
+
 export default Timeline;
