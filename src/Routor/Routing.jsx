60e65318c19a13e6eb9f60aab679296d1e5719c4
TARUN <77455621+taranshk@users.noreply.github.com>
2023-04-01 22:41:45 +0530

    Update Routing.jsx

--- src/Routor/Routing.jsx
+++ src/Routor/Routing.jsx
@@ -1,16 +1,33 @@
+// Import necessary modules from React and React Router
 import React from "react";
-import {Routes, Route} from "react-router-dom";
-import Dashboard from "./Dashboard";
-import over from "./overview";
-const MenuRouter=()=>{
-    return(
-        <Routes>
+import {Routes, Route, BrowserRouter} from "react-router-dom";
+import Dashboard from "../Pages/Dashboard";
 
-        <Route exact path='/' component={Dashboard}/>
-        <Route  path='/over' component={over}/>
-        </Routes>
-      
-       
-    );
-};
+import Navigation from "../componets/Nav/Navigation";
+
+// Define a functional component called MenuRouter
+const MenuRouter=()=>{
+return(
+// Render a BrowserRouter component from React Router
+<BrowserRouter>
+{/* Render a Routes component from React Router */}
+<Routes>
+{/* Render a Route component from React Router with a path of '/' and an element of Navigation */}
+<Route path="/" element={<Navigation/>}>
+{/* Render a Route component from React Router with an index and an element of Dashboard */}
+<Route index element={<Dashboard/>}/>
+{/* Render a Route component from React Router with a path of '/overview' and an element of over */}
+<Route path="/overview" element={<over/>}/>
+{/* Render a Route component from React Router with a path of '/analytics' and a component of commingsoon*/}
+<Route path='/analytics' component={<commingsoon/>}/>
+{/* Close the first Route component */}
+</Route>
+{/* Close the Routes component */}
+</Routes>
+{/* Close the BrowserRouter component */}
+</BrowserRouter>
+// Close the functional component
+);
+}
+// Export the MenuRouter component as the default export
 export default MenuRouter();
