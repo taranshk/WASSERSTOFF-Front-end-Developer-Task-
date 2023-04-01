ccdeaaf9fcd28c8321396b9a543d2d3f6f2c4d92
TARUN <77455621+taranshk@users.noreply.github.com>
2023-04-01 22:41:26 +0530

    Update Dashboard.jsx

--- src/Routor/Dashboard.jsx
+++ src/Routor/Dashboard.jsx
@@ -1,26 +1,41 @@
+// Import necessary modules from React and Ant Design
 import React from "react";
 import {Layout} from "antd";
 import {Content} from "antd/es/layout/layout";
 import Sider from "antd/es/layout/Sider";
+
+// Import necessary components
 import CardHover from "../componets/Sider/Sider";
 import DashboardContent from "../componets/Circle/Circle";
 import BelowTimeline from "../componets/BelowTimeline/Belowtimeline";
 import FloatButtonn from "../componets/FloatButton/Floatbutton";
-function Dashboard (){
-  return(
-         <Layout >
-        
-            <Sider theme="light" style={{height: '60vh'}}><CardHover/></Sider>
 
-         <Layout >
-           <Content>
-            <DashboardContent/>
-           <BelowTimeline/>
-           <FloatButtonn/>
-           </Content>
-         </Layout >
-   
-         </Layout>
-);
-}
+// Define a functional component called Dashboard
+const Dashboard =()=>{
+// Return the following JSX code
+return (
+<Layout >
+{/* Render a Sider component from Ant Design with a theme of "light" */}
+<Sider theme="light" style={{ minWidth: 301 }}>
+  <CardHover />
+</Sider>
+{/* Render a Layout component from Ant Design */}
+<Layout >
+{/* Render a Content component from Ant Design */}
+<Content>
+{/* Render a DashboardContent component */}
+<DashboardContent/>
+{/* Render a BelowTimeline component */}
+<BelowTimeline/>
+{/* Render a Floatbutton component */}
+<FloatButtonn/>
+</Content>
+{/* Close the Layout component */}
+</Layout >
+{/* Close the Layout component */}
+</Layout>
+// Close the functional component
+)
+};
+// Export the Dashboard component as the default export
 export default Dashboard;
