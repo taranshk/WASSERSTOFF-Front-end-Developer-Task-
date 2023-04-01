b52dac209b7dc58d5e37e95f4773363177c639d6
TARUN <77455621+taranshk@users.noreply.github.com>
2023-04-01 23:48:21 +0530

    Update SubCard3.jsx

--- src/componets/SubCard3/SubCard3.jsx
+++ src/componets/SubCard3/SubCard3.jsx
@@ -1,8 +1,13 @@
+// Importing necessary components and styles
 import { Table } from 'antd';
-import {MdArrowDropUp} from 'react-icons/md';
-import {MdArrowDropDown} from 'react-icons/md';
+import { MdArrowDropUp } from 'react-icons/md';
+import { MdArrowDropDown } from 'react-icons/md';
 import "./subcard3.css";
+
+// Destructuring Column and ColumnGroup from Table
 const { Column, ColumnGroup } = Table;
+
+// Defining an array of data for the table
 const data = [
   {
     key: '1',
@@ -26,14 +31,24 @@ const data = [
     icon: <MdArrowDropUp className="my-icon-up"/> ,
   },
 ];
+
+// Defining the SubCard3 component
 const SubCard3 = () => (
+  // Rendering a Table component with data and classNames
   <Table dataSource={data} className="my-table" pagination={false} >
+    {/* Defining a ColumnGroup to group Columns */}
     <ColumnGroup >
+      {/* Defining a Column for the 'title' property */}
       <Column  dataIndex="title" key=" title" />
-      <Column  dataIndex="lastYear" key="lastYear" />
+      {/* Defining a Column for the 'lastYear' property with a className */}
+      <Column  className='lasty' dataIndex="lastYear" key="lastYear" />
+      {/* Defining a Column for the 'PresentYear' property */}
       <Column  dataIndex="PresentYear" key="PresentYear" />
+      {/* Defining a Column for the 'icon' property */}
       <Column  dataIndex="icon" key="icon"  />
     </ColumnGroup>
   </Table>
 );
+
+// Exporting the SubCard3 component for use in other parts of the application
 export default SubCard3;
