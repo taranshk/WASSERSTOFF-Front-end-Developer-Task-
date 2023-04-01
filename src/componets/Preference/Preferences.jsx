65edc55b7c3fc7e7399be71673f6390e8559e70e
TARUN <77455621+taranshk@users.noreply.github.com>
2023-04-01 23:36:14 +0530

    Update Preferences.jsx

--- src/componets/Preference/Preferences.jsx
+++ src/componets/Preference/Preferences.jsx
@@ -1,17 +1,28 @@
+// Import the React library
 import React from "react";
+
+// Import the stylesheet for the Preferences component
 import "./preferences.css";
+
+// Import the required icons from the react-icons library
 import {AiOutlineUser} from 'react-icons/ai';
 import {HiAdjustmentsHorizontal} from 'react-icons/hi2';
-const  Preferences =()=>(
- <div className="preferences__wrapper">
-   <ul>
-    <li>
-    <AiOutlineUser/>
-    </li>
-    <li>
-    <HiAdjustmentsHorizontal/> 
-    </li>
-   </ul>
- </div>
+
+// Declare the Preferences component as a functional component
+const Preferences =()=>(
+
+  // Render the component's HTML elements
+  <div className="preferences__wrapper">
+    <ul>
+      <li>
+        <AiOutlineUser/>  {/*Render the AiOutlineUser icon*/}
+      </li>
+      <li>
+        <HiAdjustmentsHorizontal/>  {/*Render the HiAdjustmentsHorizontal icon*/}
+      </li>
+    </ul>
+  </div>
 )
+
+// Export the Preferences component for use in other parts of the application
 export default Preferences;
